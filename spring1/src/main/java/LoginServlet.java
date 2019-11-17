f37f85a6a56f831f7408949f119d2168d5340373
Manikumar Gudivada <manikumargudivada@gmail.com>
2019-11-17 07:18:08 +0530

    17Nov18

---
+++ spring1/src/main/java/LoginServlet.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/Login.do")
+public class LoginServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doGet(req, resp);
+		System.out.println("login servlet");
+		request.getRequestDispatcher("/WEB-INF/view/Login.jsp").forward(request, response);
+//		request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+		System.out.println("DIspatcher complete");
+		
+	}
+}
